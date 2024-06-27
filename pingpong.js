9718446ccfe0b36061cf2c4630dd0416b29c5964
henrycorinthians <168777174+henrycorinthians@users.noreply.github.com>
2024-06-27 17:41:13 -0300

    cria Bolina

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,7 @@
+//aqui vou criar a bolinha 
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -7,4 +11,11 @@ function draw(){
     //aqui vou por a cor da "mesa"
     //a cor da mesa é em rgb 
     background(0,0,255);
+
+    criaBolinha(xBolinha, yBolinha, tamBolinha)
+}
+
+//funcao cria bolinha 
+function criaBolinha (xBolinha, yBolinha, tamBolimha){
+
 }
